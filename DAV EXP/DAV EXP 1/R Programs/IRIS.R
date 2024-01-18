f3b3731267eff0f2831fa772d844a22df6abfe9c
Yash Ashok Shirsath <97459174+Yash22222@users.noreply.github.com>
2024-01-18 14:51:27 +0530

    IRIS

---
+++ DAV EXP/DAV EXP 1/R Programs/IRIS.R
@@ -0,0 +1,7 @@
+library("ggplot2")
+
+# Create a scatter plot
+ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) +
+  geom_point(aes(color = Species)) +
+  scale_color_viridis_d() +
+  theme_minimal()
